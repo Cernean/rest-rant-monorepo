979fe95ba60cb0fc209e3258e9c55ee96684f645
Constantin Cernean <ccernean@yahoo.com.com>
2023-05-23 19:39:05 -0400

    Using JWTs

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -29,11 +29,12 @@ function LoginForm() {
 
         if (response.status === 200) {
             setCurrentUser(data.user)
+            localStorage.setitem('token', data.token)
+            console.log(data.token)
             history.push(`/`)
         } else {
             setErrorMessage(data.message)
         }
-    
 }
 return (
     <main>
